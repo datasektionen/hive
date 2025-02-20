5dc8fb3a1bf2c699122fb97baa963ea8f528f29f
Rafael Oliveira <rmfseo@kth.se>
2025-02-20 21:32:19 +0100

    feat: add support for askama templates

--- src/web.rs
+++ src/web.rs
@@ -1,10 +1,18 @@
+use askama_rocket::Template;
+
 use crate::{errors::AppResult, routing::RouteTree};
 
 pub fn tree() -> RouteTree {
     rocket::routes![hello].into()
 }
 
+#[derive(Template)]
+#[template(path = "hello.html.j2")]
+struct HelloTemplate<'a> {
+    name: &'a str,
+}
+
 #[rocket::get("/hello")]
-async fn hello() -> AppResult<&'static str> {
-    Ok("hello")
+async fn hello() -> AppResult<HelloTemplate<'static>> {
+    Ok(HelloTemplate { name: "John" })
 }
