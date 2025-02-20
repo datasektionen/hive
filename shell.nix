7a5fad183486bae85dac86dbc97fecd80c8d8054
Rafael Oliveira <rmfseo@kth.se>
2025-02-20 13:41:10 +0100

    chore: initialize

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{
+  pkgs ? (import <nixpkgs> { }),
+  unstable ? (import <unstable> { }),
+}:
+pkgs.mkShellNoCC {
+  buildInputs = with pkgs; [
+    rustc
+    cargo
+    rustfmt
+
+    rust-analyzer
+    gcc # rust-analyzer needs cc linker
+  ];
+
+  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+}
