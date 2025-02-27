ec5c2fbaae9bbf1b53faf2d49c99ae71da167ece
Rafael Oliveira <rmfseo@kth.se>
2025-02-27 23:46:23 +0100

    feat: allow toggling UI theme

---
+++ static/main.js
@@ -0,0 +1,24 @@
+const persistedTheme = localStorage.getItem("theme");
+
+if (persistedTheme === "dark" || persistedTheme === "light") {
+  document.documentElement.dataset.theme = persistedTheme;
+}
+
+function getThemePref() {
+  if (
+    window.matchMedia &&
+    window.matchMedia("(prefers-color-scheme: dark").matches
+  ) {
+    return "dark";
+  } else {
+    return "light";
+  }
+}
+
+function toggleTheme() {
+  const current = document.documentElement.dataset.theme ?? getThemePref();
+  const other = current === "dark" ? "light" : "dark";
+
+  document.documentElement.dataset.theme = other;
+  localStorage.setItem("theme", other);
+}
