b6a3cadb7fcada05750f7945204e27dd590d0dde
Rafael Oliveira <rmfseo@kth.se>
2025-03-01 14:29:36 +0100

    feat: add favicon

--- src/web.rs
+++ src/web.rs
@@ -1,7 +1,15 @@
+use rocket::{response::Redirect, uri};
+
 use crate::routing::RouteTree;
 
 mod groups;
 
 pub fn tree() -> RouteTree {
-    RouteTree::Branch(vec![groups::routes()])
+    RouteTree::Branch(vec![groups::routes(), rocket::routes![favicon].into()])
+}
+
+#[rocket::get("/favicon.ico")]
+async fn favicon() -> Redirect {
+    // browsers expect favicon at root; redirect to real path
+    Redirect::permanent(uri!("/static/icons/favicon.ico"))
 }
